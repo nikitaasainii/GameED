f692c5d4695e8b82050abec4ecb00608a8faea59
Nikita Saini <nikitaasainii@Nikitas-MacBook-Air.local>
2026-04-27 02:51:19 +0530

    fix: clip animal sounds to 1 second

--- app/games/bubble-letters/page.js
+++ app/games/bubble-letters/page.js
@@ -82,7 +82,7 @@ function playAnimalSound(word) {
   setTimeout(() => {
     audio.pause();
     audio.currentTime = 0;
-  }, 100); // stop after 1 second
+  }, 700); // stop after 1 second
 }
 
 export default function BubbleLetters() {
